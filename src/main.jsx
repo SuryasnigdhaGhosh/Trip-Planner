eb0392d8ad81aef6b935f2b474bc34e231afacc0
Surya <suryasnigdhaghosh@gmail.com>
2025-02-06 15:40:30 +0530

    regular commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TripPlanner from './trip-planner/TripPlanner'
 import Navbar from './components/custom/Navbar'
 import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <BrowserRouter >
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/trip-planner" element={<TripPlanner />} />
-      </Routes>
-      <Toaster position="top-right"
-        reverseOrder={false}
-      />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="<your_client_id>">
+      <Navbar />
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/trip-planner" element={<TripPlanner />} />
+        </Routes>
+        <Toaster position="top-right"
+          reverseOrder={false}
+        />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
